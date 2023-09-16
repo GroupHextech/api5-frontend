9a448398eba0e412d7b1f8e7ee74f1310a38ded0
Patrick Souza <patrickgabriel103f@gmail.com>
2023-09-16 13:36:15 -0300

    Criação da sessao about

---
+++ src/components/About.vue
@@ -0,0 +1,28 @@
+<template>
+    dfsdfsdf
+</template>
+<style scoped>
+.footer {
+  background-color: rgba(0, 0, 0, 0.181);
+  bottom: 0.5rem;
+  position: fixed;
+  width: 100%;
+  text-align: center;
+  font-size: 12px;
+  height: 20%;
+  align-items: end;
+}
+footer {
+  max-height: 50px; /* Defina a altura máxima desejada */
+  overflow: hidden; /* Para ocultar o conteúdo que excede a altura máxima */
+}
+.footer ul{
+  display: flex;
+  gap: 1px;
+}
+
+.footer ul li{
+  border-right: 2px solid rgba(0, 0, 0, 0.412);
+}
+
+</style>
