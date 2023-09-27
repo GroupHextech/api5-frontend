d49963fcae180ea0e25a80d9c0c2aae76b05ab1b
Michel Rubens <michelrube@gmail.com>
2023-09-27 19:58:44 +0000

    Add chart

--- src/views/Receitas.vue
+++ src/views/Receitas.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
     <h2>Receitas</h2>
+    <div class="card">
+      <ReceitasBarChart />
+    </div>
   </div>
 </template>
 
 <script setup>
+import ReceitasBarChart from '../charts/ReceitasBarChart.vue';
+
 </script>
 
 <style scoped></style>
