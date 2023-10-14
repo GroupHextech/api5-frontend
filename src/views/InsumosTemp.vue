8cc92d31507f372e6f4a1e45838bd37bf63e881f
Patrick Souza <patrickgabriel103f@gmail.com>
2023-10-14 18:36:42 -0300

    Add Dash

---
+++ src/views/InsumosTemp.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h2>Quadro Insumos</h2>
+    </div>
+    <div>
+      <InsumosDash></InsumosDash>
+    </div>
+  </template>
+  
+  <script setup>
+  import InsumosDash from '../components/InsumosDash.vue';
+  </script>
+  
+  <style scoped>
+</style>
