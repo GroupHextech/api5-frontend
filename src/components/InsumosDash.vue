6a34d8cdb27e26b06f163ade5c5a49a3c93c511d
Patrick Souza <patrickgabriel103f@gmail.com>
2023-10-14 18:34:13 -0300

    InsumosDash add

---
+++ src/components/InsumosDash.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <iframe
+        width="1000"
+        height="480"
+        :src="powerBIReportEmbedUrl"
+        frameborder="0"
+        allowFullScreen="true">
+      </iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        powerBIReportEmbedUrl: "https://app.powerbi.com/view?r=eyJrIjoiNTExZGExZWEtNTZkMy00ZGI1LTg0NGYtMjUzNzVhYjI1ZjE3IiwidCI6ImRkNDg5YTlkLTU4Y2EtNGI3Ny1iM2RkLWQ5MzYyZGJkMjdlZCJ9"
+      };
+    }
+  };
+  </script>
+
+  
+  
