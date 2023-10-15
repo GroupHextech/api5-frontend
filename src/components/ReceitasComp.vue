19d7a96d0a1e8720234255d080cfcc32f76a4013
Patrick Souza <patrickgabriel103f@gmail.com>
2023-10-15 15:29:06 -0300

    Alteracoes feedback

--- src/components/ReceitasComp.vue
+++ src/components/ReceitasComp.vue
@@ -1,22 +1,21 @@
 <template>
-    <div>
-      <iframe
-        width="1000"
-        height="480"
-        :src="powerBIReportEmbedUrl"
-        frameborder="0"
-        allowFullScreen="true">
-      </iframe>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        powerBIReportEmbedUrl: "https://app.powerbi.com/view?r=eyJrIjoiNTExZGExZWEtNTZkMy00ZGI1LTg0NGYtMjUzNzVhYjI1ZjE3IiwidCI6ImRkNDg5YTlkLTU4Y2EtNGI3Ny1iM2RkLWQ5MzYyZGJkMjdlZCJ9"
-      };
-    }
-  };
-  </script>
-  
+  <div>
+    <iframe
+      width="1070"
+      height="485"
+      :src="powerBIReportEmbedUrl"
+      frameborder="0"
+      allowFullScreen="true">
+    </iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      powerBIReportEmbedUrl: "https://app.powerbi.com/view?r=eyJrIjoiZjA3OTAzYWItMTgyMC00OGQwLTg5YmItYmZjODFmY2VlYjUzIiwidCI6ImRkNDg5YTlkLTU4Y2EtNGI3Ny1iM2RkLWQ5MzYyZGJkMjdlZCJ9"
+    };
+  }
+};
+</script>
--- src/views/Receitas.vue
+++ src/views/Receitas.vue
@@ -1,14 +1,75 @@
 <template>
   <div>
-    <h2>Receitas</h2>
-  </div>
-  <div>
-    <ReceitasComp></ReceitasComp>
+    <div class="container">
+      <h1>Controle de Insumos do Restaurante</h1>
+      <button @click="mostrarComponenteEntradaSaida">
+        <i class='bi bi-table icone'></i>
+        {{ textoBotao}}
+      </button>   
+    </div>
+    <div class="table-container">
+      <template v-if="mostrarEntradaSaida">
+        <EntradaSaidas></EntradaSaidas>
+      </template>
+      <template v-else>
+        <InsumosTable></InsumosTable>
+      </template>
+    </div>
   </div>
 </template>
 
-<script setup>
-import ReceitasComp from '../components/ReceitasComp.vue';
+
+
+
+<script>
+import InsumosTable from '../components/InsumosTable.vue';
+import EntradaSaidas from '../components/EntradaSaidas.vue';
+
+export default {
+  data() {
+    return {
+      mostrarEntradaSaida: false,
+      textoBotao: 'Entradas e Saídas',
+    };
+  },
+
+  components: {
+    InsumosTable,
+    EntradaSaidas
+  },
+
+
+methods: {
+    mostrarComponenteEntradaSaida() {
+      this.mostrarEntradaSaida = !this.mostrarEntradaSaida;
+      this.textoBotao = this.mostrarEntradaSaida ? 'Insumos' : 'Entradas e Saídas';
+    }
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.icone{
+  margin-right: 10px;
+}
+.container{
+  display: flex;
+  flex-direction: row;
+}
+button{
+  border-radius: 10px;
+  background-color: var(--roxohex);
+  color: rgb(0, 0, 0);
+  margin-left: 5rem;
+}
+button:hover {
+  background-color: #7c6ed66b;
+  color: #000000;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+}
+.table-container {
+  /* Defina a largura máxima para a área de rolagem horizontal, se necessário */
+  max-width: 100%;
+  
+}
+</style>
