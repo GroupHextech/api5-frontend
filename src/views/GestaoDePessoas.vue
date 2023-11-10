aed4669dc179978648d31525429401f7335e84a7
Michel Rubens <michelrube@gmail.com>
2023-11-10 20:16:50 -0300

    Creation of GestaoDePessoal page

---
+++ src/views/GestaoDePessoas.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2 class="page-title">Gestão de Pessoas</h2>
+
+    <div class="dash">
+
+    </div>
+
+    <ul>
+      <li>Escalas de trabalho</li>
+      <li>Horas trabalhadas</li>
+      <li>Folgas</li>
+      <li>Desempenho</li>
+      <li>Perfil de acesso</li>
+    </ul>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.dash {
+  height: 100%;
+}
+
+.dash,
+.dash iframe {
+  width: 100%;
+}
+</style>
