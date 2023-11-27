d0a278246cd5926e4b508d3950c251f3e7f34bae
Patrick Souza <patrickgabriel103f@gmail.com>
2023-11-26 21:01:17 -0300

    manutencao graficos

--- src/views/Fornecedores.vue
+++ src/views/Fornecedores.vue
@@ -1,24 +1,15 @@
 <template>
   <div>
-    <h2 class="page-title">Fornecedores</h2>
-
-    <div class="dash">
-      <!--FornecedorList /-->
-      <iframe title="Report Section" height="600" src="https://app.powerbi.com/view?r=eyJrIjoiNGM0NjY1MWMtMGY4NS00ODQ4LThlMzQtODM5YTdhNGJlZTg5IiwidCI6ImRkNDg5YTlkLTU4Y2EtNGI3Ny1iM2RkLWQ5MzYyZGJkMjdlZCJ9" frameborder="0" allowFullScreen="true"></iframe>
-    </div>
-
+    <h2 class="page-title">Dashboard</h2>
+  </div>
+  <div>
+    <DashFornecedor></DashFornecedor>
   </div>
 </template>
 
 <script setup>
-import FornecedorList from '../components/FornecedorList.vue';
+import DashFornecedor from '../components/DashFornecedor.vue';
 </script>
 
 <style scoped>
-.dash {
-  height: 100%;
-}
-.dash, .dash iframe {
-  width: 100%;
-}
 </style>
